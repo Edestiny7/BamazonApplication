cdbce65b0530674ec956d6bf3155ac7ff5456be6
Rose <50706764+Edestiny7@users.noreply.github.com>
2019-09-28 20:34:25 -0400

    Adding finished local code to GitHub finally

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # BamazonApplication
+Bamazon is an Amazon-like storefront that works with the command line node app, which takes in parameters and gives back data from a SQL database.  Users will be shown items available for purchase. They will specify which products they want want to purchase by product ID and quantity.  If enough stock is available, they will be told their total and thanked for their purchase.  Otherwise they will be told there is not enough available at the time, and to try back at another time. 
+
+# How to use
+1. Open a console app such as Bash.
+2. Navigate to the `bamazonCustomer.js` file in `BamazonApplication/assets/javascript/`
+3. Enter the following command:
+    * `node bamazonCustomer`
+        * Results: Items in the Bamazon inventory will be displayed along with a prompt to enter the ID of the item you would like to purchase
+            ![alt text](./assets/images/Inventory.PNG "Bamazon Inventory")
+        * You will then be prompted to enter the quantity
+            ![alt text](./assets/images/Prompt.PNG "Bamazon Prompt")
+        * If your purchase request is successful, you will be see a confirmation   
+            ![alt text](./assets/images/Confirmation.PNG "Bamazon Confirmation")
+        * If your purchase request is not successful, you will be see a cancellation   
+            ![alt text](./assets/images/Cancellation.PNG "Bamazon Cancellation")
+        * Updated Bamazon inventory will be displayed following each message returned   
+
+### [Try the App!](https://github.com/Edestiny7/BamazonAppication)
+
+Technology: JavaScript | Node.js | Git | mySQL
---
+++ assets/javascript/bamazonCustomer.js
@@ -0,0 +1,81 @@
+let mysql = require("mysql");
+let inquirer = require("inquirer");
+let table = require("cli-table");
+
+// connection info for sql database
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+// connection to mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`Connected as ID: ${connection.threadId}`);
+});
+
+// Display inventory
+function inventory() {
+    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+        if (err) {
+            console.log(err)
+        };
+
+        let display = new table({
+            head: ["ID", "Product", "Price"],
+            colWidths: [5, 75, 10]
+        });
+        for (var i = 0; i < res.length; i++) {
+            display.push(
+                [res[i].item_id, res[i].product_name, res[i].price]
+            );
+        }
+        console.log(display.toString());
+
+        // Prompt user  
+        inquire();
+    });
+}
+
+// Prompt user for selection
+let inquire = function() {
+    inquirer
+        .prompt([{
+                name: `ProductID`,
+                type: `input`,
+                message: `Please enter ID of item you would like to purchase: `,
+            },
+            {
+                name: `Quantity`,
+                type: `input`,
+                message: `Quantity? `,
+            }
+        ]).then(function(answer) {
+            let purchaseID = answer.ProductID;
+            let purchaseQty = answer.Quantity;
+            purchase(purchaseID, purchaseQty);
+        });
+}
+
+// Process request
+function purchase(ID, qty) {
+    connection.query("SELECT * FROM products WHERE item_id =" + ID, function(err, res) {
+        if (err) {
+            console.log(err)
+        };
+        if (qty <= res[0].stock_quantity) {
+            let total = res[0].price * qty;
+            console.log(`Total for ${qty} - ${res[0].product_name} is ${total}. 
+Thank you for shopping with Bamazon!`);
+            connection.query("UPDATE products SET stock_quantity = (stock_quantity - " + qty + ") WHERE item_id =" + ID);
+        } else {
+            console.log(`Unfortunately, stock of ${res[0].product_name} is insufficient to fulfill your order at this time. Please try again later.`);
+        };
+        inventory();
+    });
+};
+
+inventory();
